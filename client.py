cf842479fc0eee1d00ed3f686cba328e372304b1
Chris <chris.goringe@gmail.com>
2024-09-08 15:35:09 +1000

    use requests and fastapi

--- client.py
+++ client.py
@@ -1,15 +1,6 @@
-import torch, pickle
-from websockets.sync.client import connect
+import torch
+import requests
 import numpy as np
-
-def decode(t:torch.Tensor, server):
-    with connect(server, max_size=2**30) as websocket:
-        websocket.send("V".encode())
-        websocket.send(t.shape)
-        websocket.send(t.numpy().astype(np.float32).to_bytes())
-
-        response = websocket.recv()
-        return pickle.loads(response)
     
 class RemoteVae:
     category = "experimental"
@@ -17,12 +8,14 @@ class RemoteVae:
     def INPUT_TYPES(s):
         return {"required": {
                     "latent": ("LATENT",),
-                    "server": ("STRING", {"default":"ws://localhost:8765"}),
+                    "server": ("STRING", {"default":"https://192.168.0.119:8188"}),
                 }}
 
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "func"
 
     def func(self, latent, server):
-        image = decode(torch.Tensor(latent['samples']), server)
+        payload = { "latent" : latent['samples'].numpy().toarray() }
+        r = requests.get(server+"/decode_latent", json=payload)
+        image = torch.from_numpy( np.array(r['image']) )
         return (image,)
--- server.py
+++ server.py
@@ -1,43 +1,43 @@
-import asyncio
-from websockets.asyncio.server import serve
+
 import torch
-from safetensors.torch import load_file
 import numpy as np
 
-async def main():
-    async with serve(decode, "localhost", 8765, max_size=2**30):
-        await asyncio.get_running_loop().create_future()  # run forever
+from server import PromptServer
+from aiohttp import web
+import time
+
+routes = PromptServer.instance.routes
+@routes.post('/decode_latent')
+async def my_function(request):
+    the_data = await request.post()
+    return web.json_response({"image":RemoteVaeServer.decode(the_data)})
+
 
 class RemoteVaeServer:
-    websocket = None
+    vae     = None
+    called  = False
+
     category = "experimental"
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": { 
-                "vae": ("VAE", {"tooltip": "The VAE model used for decoding the latent."})
+                "vae": ("VAE", {"tooltip": "The VAE model used for decoding the latent."}),
             }}
 
     RETURN_TYPES = ()
     FUNCTION = "func"
     OUTPUT_NODE = True
 
-    def __init__(self):
-        pass
-
     def func(self, vae):
-        command = self.websocket.recv().decode()
-        if command=="V":
-            shape = self.websocket.recv()
-            raw = self.websocket.recv()
-            latent_samples = torch.from_numpy( np.ndarray(shape=shape, dtype=np.float32, buffer=raw) )
-            image:torch.Tensor = vae.decode(latent_samples)
-            self.websocket.send(image.shape)
-            self.websocket.send(image.numpy().to_bytes())
+        RemoteVaeServer.vae = vae
+        self.called = False 
+        while not self.called: time.sleep(10)
         return ()
-        
-
-def decode(websocket):
-    RemoteVaeServer.websocket = websocket
     
-asyncio.run(main())
+    @classmethod
+    def decode(cls, dict):
+        latent_samples = torch.from_numpy( np.array( dict['latent' ]) )
+        image:torch.Tensor = cls.vae.decode(latent_samples.cuda()).cpu()
+        cls.called = True
+        return image.numpy().tolist()
